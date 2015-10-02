fcfa30e7aafcbd9f80f2acdaac5add4ddf44edcc
unknown <x9zou@ece-rtos3.NEXUS.UWATERLOO.CA>
2015-10-02 18:30:05 -0400

    lab2 part1 finished

--- lab2/starter/RTX_CM3/SRC/CM/rt_Task_ext.c
+++ lab2/starter/RTX_CM3/SRC/CM/rt_Task_ext.c
@@ -29,23 +29,78 @@
  *---------------------------------------------------------------------------*/
 /*--------------------------- rt_tsk_count_get ------------------------------*/
 /* added in ECE254 lab2 keil_rtx */
+
 int rt_tsk_count_get (void) {
-	/* Add your own code here. Change the following line accordingly */
-	return 0;
+	int activeNum;
+	int count;
+	activeNum = 0;
+
+	for (count = 1; count <= os_maxtaskrun; count++) {
+    if (os_active_TCB[count-1] != NULL) {
+      activeNum++;
+    }
+  }
+
+	if(os_idle_TCB.state != INACTIVE){
+		activeNum++;
+	}
+	
+	return activeNum;
 }
+
+
+
 
 /*--------------------------- rt_tsk_get ------------------------------------*/
 /* added in ECE254 lab3 keil_proc */
 OS_RESULT rt_tsk_get (OS_TID task_id, RL_TASK_INFO *p_task_info) {
-	/* Add your own code here. Change the following lines accordingly */
-	p_task_info->task_id     = task_id;
-	p_task_info->state       = INACTIVE;
-	p_task_info->prio        = 99;
-	p_task_info->stack_usage = 15;
-	p_task_info->ptask       = NULL;
+	
+	/* sanity check */
+    if( ((task_id < 1 || task_id > os_maxtaskrun) && task_id != 0xFF)||
+		((P_TCB)os_active_TCB[task_id - 1] == NULL)||
+		(p_task_info == NULL || t == NULL))
+		return OS_R_NOK;
+
+	/*	Get TCB of this task id info  */
+    P_TCB p_task;
+    p_task = os_active_TCB[task_id-1];
+
+	
+    /*	task_id  */
+	p_task_info->task_id     = p_task->task_id;
+	
+	/*	task_state  */
+	p_task_info->state       = p_task->state;
+
+	/*	execution priority  */
+	p_task_info->prio        = p_task->prio;
+    
+	/*  ptask   */
+	p_task_info->ptask       = p_task->ptask;
+
+
+    /*  stack usage   */
+	U32 stackSize, currAddr, endAddr;
+	U32 *sEnd;
+
+    // stack size
+	stackSize = p_task->priv_stack >> 2;
+    if(stackSize == 0)
+		stackSize = (U16)os_stackinfo >> 2;
+
+    // end address
+	sEnd = &p_task->stack[stackSize];
+	endAddr = (U32)sEnd;
+
+    // current address  
+	// to do if not running)
+	currAddr = p_task->tsk_stack;
+
+    p_task_info->stack_usage = (U32)(endAddr - currAddr)*100/(U16)osstackinfo;
 	return OS_R_OK;
+
+
+
 }
 
-/*----------------------------------------------------------------------------
- * end of file
- *---------------------------------------------------------------------------*/
+
